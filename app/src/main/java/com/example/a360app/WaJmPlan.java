956bcd27019f4c20ce816e7a88cea480dd8f2227
CarlOcsan <c.ocsan010@mybvc.ca>
2023-02-27 16:49:51 -0700

    vskc

---
+++ app/src/main/java/com/example/a360app/WaJmPlan.java
@@ -0,0 +1,14 @@
+package com.example.a360app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WaJmPlan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wa_jm_plan);
+    }
+}
