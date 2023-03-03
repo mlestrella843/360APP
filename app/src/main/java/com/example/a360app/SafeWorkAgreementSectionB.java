3fc7ada8b58f56f3f7880992cf6dd9860d1e2a97
Justin Tremblett <97629125+JTremblettBVC@users.noreply.github.com>
2023-03-02 21:18:06 -0700

    SWAP broken. gonna work on it when I get caught up in android

---
+++ app/src/main/java/com/example/a360app/SafeWorkAgreementSectionB.java
@@ -0,0 +1,14 @@
+package com.example.a360app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SafeWorkAgreementSectionB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safe_work_agreement_section_b);
+    }
+}
